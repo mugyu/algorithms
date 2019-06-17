f38e7aadec96633d498886b9cdf68d628f037b65
mugyu <zcbyxvzz@gmail.com>
2019-06-18 08:59:55 +0900

    fix randmized_quick_sort.rb: dose not work

--- ruby/randmized_quick_sort.rb
+++ ruby/randmized_quick_sort.rb
@@ -1,49 +1,52 @@
-# does not work
+# クイックソート
 class Array
   def qsort!
     quicksort(0, self.size - 1)
   end
 
   def quicksort(left, right)
-    if left < right
-      p = left
-      k = left + 1
-      while k <= right
-        if self[k] < self[left]
-          self[p + 1], self[k] = self[k], self[left]
-          p += 1
-        end
-        self[left], self[p] = self[p], self[left]
-        quicksort(left, p - 1)
-        quicksort(p + 1, right)
+    return if left >= right
+    p = left
+    k = left + 1
+    while k <= right
+      if self[k] < self[left]
+        p += 1
+        self[p], self[k] = self[k], self[p]
       end
+      k += 1
     end
+    self[left], self[p] = self[p], self[left]
+    quicksort(left, p)
+    quicksort(p + 1, right)
   end
 
   def rqsort!
     randomized_quicksort(0, self.size - 1)
   end
 
+  # あまりランダム無にメリットはないけど
+  # 初期の並び順が最悪の場合の助かるかも
+  # ただ、ランダムにピポットを選択した結果、最悪になるかもしれない
+  # 結局運次第か
   def randomized_quicksort(left, right)
-    if left < right
-      r = rand(right - left + 1) + left
-      self[left], self[r] = self[r], self[left]
-      p = left
-      k = left + 1
-      while k <= right
-        if self[k] < self[left]
-          self[p + 1], self[k] = self[k], self[p + 1]
-          p += 1
-        end
-        k += 1
+    return if left >= right
+    r = rand(right - left) + left + 1
+    self[left], self[r] = self[r], self[left]
+    p = left
+    k = left + 1
+    while k <= right
+      if self[k] < self[left]
+        p += 1
+        self[p], self[k] = self[k], self[p]
       end
-      self[left], self[k] = self[k], self[left]
-      randomized_quicksort(left, p - 1)
-      randomized_quicksort(p + 1, right)
+      k += 1
     end
+    randomized_quicksort(left, p)
+    randomized_quicksort(p + 1, right)
   end
 end
 
 a = [rand(9), rand(9), rand(9), rand(9), rand(9), rand(9), rand(9), rand(9)]
-a.qsort!
+p a
+a.rqsort!
 p a
