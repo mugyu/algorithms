fd1e6199c495a52cde4cd956107111b03fea5bad
mugyu <zcbyxvzz@gmail.com>
2019-06-20 08:47:32 +0900

    insertsort.rb: construction done

--- ruby/insertsort.rb
+++ ruby/insertsort.rb
@@ -1,21 +1,25 @@
-# 作りかけ
 # インサートソート
 require "pp"
 data = [3, 9, 6, 1, 2]
 
 def insertSort(data)
-  length = data.length
-  length.times do |i|
-    next if i < 1
-    length.times do |j|
-      break if j > i
-      if data[j] < data[i]
-        value = data[j]
-        data[j]  = data[i]
-        data[i] = value
-      end
+  length = data.length - 1
+
+  (1..length).each do |i|
+    (0...i).each do |j|
+      # exclude
+      next if data[j] <= data[i]
+      # memo
+      value = data[i]
+      # shift
+      (j...i).reverse_each { |x| data[x + 1] = data[x] }
+      # insert
+      data[j] = value
+      p data
     end
   end
+  data
 end
 
-insertSort(data)
+p data
+p insertSort(data)
