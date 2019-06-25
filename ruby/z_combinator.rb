aee060b71ef57132fe28f430f1d5d1680a17d950
mugyu <zcbyxvzz@gmail.com>
2019-06-25 11:13:21 +0900

    change to use lambda litral

--- ruby/z_combinator.rb
+++ ruby/z_combinator.rb
@@ -2,12 +2,23 @@
 #https://ja.wikipedia.org/wiki/%E4%B8%8D%E5%8B%95%E7%82%B9%E3%82%B3%E3%83%B3%E3%83%93%E3%83%8D%E3%83%BC%E3%82%BF#Z%E3%82%B3%E3%83%B3%E3%83%93%E3%83%8D%E3%83%BC%E3%82%BF
 # Z =  λf . (λx . f (λy . x x y)) (λx . f (λy . x x y))
 #値渡し評価（適用順序）でも使用可能なバージョンのYコンビネータは、通常のYコンビネータの一部をη変換することで与えられる。
+
+# lambdaリテラル(lambda litral)で書くとこう
+# * 複数行の場合はlambda式で書く
+# * .call() で書く方が良いらしいけど、まとまっている所は簡便に.()
 def Z(func)
   lambda do |f|
-      lambda{ |x| lambda{ |y| f[x[x]][y] } }[
-        lambda{ |x| lambda{ |y| f[x[x]][y] } }
-      ]
-  end[func]
+    ->(x) { ->(y) { f.(x.(x)).(y) } }.call(
+      ->(x) { ->(y) { f.(x.(x)).(y) } }
+    )
+  end.call(func)
+
+# []でcallする場合
+# lambda do |f|
+#     lambda{ |x| lambda{ |y| f[x[x]][y] } }[
+#       lambda{ |x| lambda{ |y| f[x[x]][y] } }
+#     ]
+# end[func]
 # .callメソッドで書くとこう
 # lambda do |f|
 #     lambda{ |x|lambda{ |y| f.call(x.call(x)).call(y) } }.call(
@@ -16,7 +27,7 @@ def Z(func)
 end
 
 # フィボナッチ数
-fib = Z(lambda{ |f| lambda{ |n| n < 2 ? n : f[n - 2] + f[n - 1] } })
+fib = Z( ->(f) { ->(n) { n < 2 ? n : f.call(n - 2) + f.call(n - 1) } } )
 (1..10).each do |n|
   p fib.call(n)
 end
