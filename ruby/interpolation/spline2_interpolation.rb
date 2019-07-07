15f722d6b1704c60003d0ed2f0744d2d6a80bdc5
mugyu <zcbyxvzz@gmail.com>
2019-07-08 08:35:45 +0900

    Improve

--- ruby/interpolation/spline2_interpolation.rb
+++ ruby/interpolation/spline2_interpolation.rb
@@ -1,59 +1,61 @@
 # 3次Spline補間
+#  線形補間になっている気がするので後で調べる
 class Spline
-  def initialize(x, y)
-    @x = x
-    @y = y
-    n = x.size
-    d = [0]
+  def initialize(dataX, dataY)
+    @x = dataX
+    @y = dataY
 
-    1.upto(n) do |i|
-      h[i] =  x[i] - x[i - 1]                          # 間隔
-      d[i] = (y[i] - y[i - 1]) / h[i]                  # 一次微分
+    @length = @x.length - 1
+
+    # 間隔
+    @h = Array.new(20, 0.0)
+    # 一次微分
+    dif1 = Array.new(20, 0.0)
+    # 二次微分
+    @dif2 = Array.new(20, 0.0)
+
+    (1..@length).each do |i|
+      # 間隔を計算
+      @h[i] = @x[i] - @x[i - 1]
+      # 一次微分
+      dif1[i] = Rational(@x[i] - @x[i - 1], @h[i])
     end
 
-    1.upto(n) do |i|
-      d[i] = (d[i + 1] - d[i]) / (x[i + 1] - x[i - 1]) # 二次微分
-    end
-
-    while x x[n - 1]
-      x += 0.01
+    # 二次微分
+    (1..@length).each do |i|
+      @dif2[i] = Rational(dif1[i] - dif1[i - 1], @x[i] - @x[i - 1])
     end
   end
 
-  def interpolate(t)
+  def calc
+    i = 1
+    result = []
+    x = @x[0]
+    while x < @x[@length]
+      # 第１項
+      yy0 = Rational(@dif2[i - 1], 6 * @h[i]) \
+            * (@x[i] - x) * (@x[i] - x) * (@x[i] - x)
+      # 第２項
+      yy1 = Rational(@dif2[i], 6 * @h[i]) \
+            * (x - @x[i - 1]) * (x - @x[i - 1]) * (x - @x[i - 1])
+      # 第３項
+      yy2 = (Rational(@y[i - 1], @h[i]) - Rational(@h[i] * @dif2[i - 1], 6)) \
+            * (@x[i] - x)
+      # 第４項
+      yy3 = (Rational(@y[i], @h[i]) - Rational(@h[i] * @dif2[i], 6)) \
+            * (x - @x[i - 1])
+      y = yy0 + yy1 + yy2 + yy3
+      result.push([x, y.to_f])
+      i += 1 if x >= @x[i]
+      x += 0.1r
+    end
+    result
   end
 end
 
-# This is probably c or c++ or c# or java language code
-#
-#    int i;
-#    double x,y,yy0,yy1,yy2,yy3;
-#    double[] h=new double[20];     //間隔
-#    double[] dif1=new double[20];  //一次微分
-#    double[] dif2=new double[20];  //二次微分
-#
-#    h[0]=0.0;
-#    dif2[0]=0.0;
-#    dif2[data_count-1]=0.0;
-#
-#    Graphics g=getGraphics();
-#    g.setColor(new Color(0,192,192));  //dark cyanで線を引く
-#
-#    i=1;
-#    for(x=dataX[0];x<dataX[data_count-1];x+=0.01){
-#       if(x<dataX[i]){
-#          yy0=dif2[i-1]/(6*h[i])*(dataX[i]-x)
-#             *(dataX[i]-x)*(dataX[i]-x);       //第１項
-#          yy1=dif2[i]/(6*h[i])*(x-dataX[i-1])
-#             *(x-dataX[i-1])*(x-dataX[i-1]);   //第２項
-#          yy2=(dataY[i-1]/h[i]-h[i]*dif2[i-1]/6)
-#             *(dataX[i]-x);                    //第３項
-#          yy3=(dataY[i]/h[i]-h[i]*dif2[i]/6)*
-#             (x-dataX[i-1]);                   //第４項
-#          y=yy0+yy1+yy2+yy3;
-#          g.drawRect(X0+(int)(10*x),Y0+(int)(10*(40-y)),0,0);
-#       }
-#       else i++;
-#    }
-#
-# }
+x = [1, 2, 3, 4, 5]
+y = [2.0, 1.1, 2.4, 3.2, 5.9]
+s = Spline.new(x, y)
+s.calc.each do |_, yy|
+  puts yy
+end
